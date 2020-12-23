e219439b472cd680832f128e30e15c389b6304aa
zaifleitas <zai.fleitas@gmail.com>
2020-12-22 22:51:28 -0300

    Login-register

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,12 +1,17 @@
+import login.UserPasswordEncoderListener
 import com.fuini.sd.web.rest.Category.CategoryResourceImpl
 import com.fuini.sd.web.rest.Product.ProductResourceImpl
 import com.fuini.sd.web.service.Category.CategoryServiceImpl
 import com.fuini.sd.web.rest.Supplier.SupplierResourceImpl
 import com.fuini.sd.web.service.Product.ProductServiceImpl
 import com.fuini.sd.web.service.Supplier.SupplierServiceImpl
+import login.CustomUserDetailsService
 
 // Place your Spring DSL code here
 beans = {
+    userPasswordEncoderListener(UserPasswordEncoderListener)
+    userDetailsService(CustomUserDetailsService)
+
     //Resources
     categoryResource(CategoryResourceImpl)
     supplierResource(SupplierResourceImpl)
