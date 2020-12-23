762c19908246adeeaa4efcd611a634371e62ef4c
Adrian Holman <adriholman@hotmail.com>
2020-12-23 17:36:06 -0300

    Update resources.groovy

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -12,10 +12,10 @@ import com.fuini.sd.web.service.Supplier.SupplierServiceImpl
 beans = {
     //Resources
     categoryResource(CategoryResourceImpl)
-    roleResource(RoleResourceImpl)
     supplierResource(SupplierResourceImpl)
     productResource(ProductResourceImpl)
     purchaseRecordResource(PurchaseRecordResourceImpl)
+    roleResource(RoleResourceImpl)
 
     //Services
     categoryService(CategoryServiceImpl)
